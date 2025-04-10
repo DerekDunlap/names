52b36db47ef3ac3aeb638688a454af68cde5280c
Derek <derekdunlapjr@gmail.com>
2025-04-09 21:38:55 -0400

    Initial commit

---
+++ names.js
@@ -0,0 +1,18 @@
+var xhr;
+
+if(window.xmlhttpRequest){
+    xhr = new XMLHttpRequest();
+}else{
+    xhr = new ActiveXObject("Microsoft.XMLHTTP");
+}
+
+requestURL = "https://api.sheetbest.com/sheets/c1e0ead6-6df0-49f7-ace0-ec90562a8c3f"
+
+xhr.readystatechange = function(){
+    if (xhr.readyState == 4 && xhr.status == 200){
+        document.getElementById("namearea").innerHTML = xhr.responseText;
+    }
+}
+
+xhr.open("GET", requestURL, true);
+xhr.send();
